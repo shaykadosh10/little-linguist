41890f7017b833a52d61e70f3dae1ad182042aa3
ShayKadosh <shaykadosh10@gmail.com>
2024-02-26 19:22:58 +0200

    change name

--- src/app/components/categories-table/categories-table.component.spec.ts
+++ src/app/components/categories-table/categories-table.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CategoriesTableComponent } from './categories-table.component';
 
 describe('CategoriesTableComponent', () => {
